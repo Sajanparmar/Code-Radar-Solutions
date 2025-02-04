0dc10a214421dc7165ca7973a54db28684476f64
Sajanparmar <parmarsajan707@gmail.com>
2025-02-04 10:26:10 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 int main() {
-    int x;
-    scanf("%d",&x);
-    if (x==1 || x==3 ||x==5 || x==7|| x==8 || x==10 || x==12){
-        printf("31");
+    int x,y,calculation;
+    char op;
+    scanf("%d %d %c",&x ,&y,&op);
+    if(op=='+'){
+        calculation =x+y;
     }
-    else if(x==2){
-        printf("28");
+    else if(op=='-'){
+        calculation = x-y;
     }
-    else if(x==4 || x==6||x==9 ||x==11){
-        printf("30");
-    }
-    else{
-        printf("Invalid month");
-    }
+    else if(op=='')
     return 0;
 }
